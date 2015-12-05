434f5079edc3698c4908759bda5f7143e142c118
Dmitry Orlov <me@mosquito.su>
2015-12-05 11:30:18 +0300

    [fea] release 1.0

--- tornado_xmlrpc/__init__.py
+++ tornado_xmlrpc/__init__.py
@@ -13,7 +13,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 6)
+version_info = (1, 0)
 
 __version__ = ".".join(map(str, version_info))
 __author__ = ", ".join("{0} <{1}>".format(*author) for author in author_info)
