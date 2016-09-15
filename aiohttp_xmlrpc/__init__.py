37e70e02311742b3aa1ed0ad73291844b5fbd5b9
Dmitry Orlov <me@mosquito.su>
2016-09-15 21:15:02 +0300

    bump

--- aiohttp_xmlrpc/__init__.py
+++ aiohttp_xmlrpc/__init__.py
@@ -11,7 +11,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 3, 5)
+version_info = (0, 3, 6)
 description = "aiohttp XML-RPC server handler and client"
 
 __version__ = ".".join(map(str, version_info))
