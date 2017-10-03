02aee5cce7cc4cff6af74dc7e040919ec3498a9c
Sergey Naumenko <Sergey.Naumenko@acronis.com>
2017-10-03 18:27:59 +0300

    [fea] Bump version

--- aiohttp_xmlrpc/__init__.py
+++ aiohttp_xmlrpc/__init__.py
@@ -11,7 +11,7 @@ author_info = [
     ("Dmitry Orlov", "me@mosquito.su")
 ]
 
-version_info = (0, 4, 4)
+version_info = (0, 4, 5)
 description = "aiohttp XML-RPC server handler and client"
 
 __version__ = ".".join(map(str, version_info))
