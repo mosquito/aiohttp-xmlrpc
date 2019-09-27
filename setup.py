e3115eaf69df0c8e7d09714e4f1d072cb915109f
Alexander Tolkachev <alexander.tolkachev@acronis.com>
2019-09-27 18:37:56 +0300

    Add possibility to work with '%Y%m%dT%H%M%S' datetime format

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     include_package_data=True,
